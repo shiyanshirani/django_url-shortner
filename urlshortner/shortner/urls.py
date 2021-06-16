b28e5e3b9fb279416b9dc1b38826e7019f5b133f
Shiyan Shirani <shiyan99s@gmail.com>
2021-06-16 09:32:39 +0530

    url shortner added

---
+++ urlshortner/shortner/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('create', views.create, name='index'),
+    path('<str:pk>', views.go, name='go')
+]
